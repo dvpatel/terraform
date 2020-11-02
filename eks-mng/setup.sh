8c17799e82101a697c674afef8b82c89370cf122
Dipesh Patel <mr.dvpatel@gmail.com>
2020-11-01 21:06:02 -0500

    update sample app

--- eks-mng/setup.sh
+++ eks-mng/setup.sh
@@ -19,9 +19,11 @@ kubectl -n kube-system annotate deployment.apps/cluster-autoscaler cluster-autos
 
 kubectl -n kube-system set image deployment.apps/cluster-autoscaler cluster-autoscaler=us.gcr.io/k8s-artifacts-prod/autoscaling/cluster-autoscaler:v1.18
 
-kubectl apply -f web-app.yaml
-kubectl get deployment/web-stateless
-kubectl get deployment/web-stateful
 
-kubectl scale --replicas=5 deployment/web-stateless
-kubectl get pods
+kubectl create namespace dev
+kubectl apply -f web-app.yaml
+kubectl get -n dev deployment/web-stateless
+kubectl get -n dev deployment/web-stateful
+
+kubectl scale -n dev --replicas=5 deployment/web-stateless
+kubectl get -n dev pods

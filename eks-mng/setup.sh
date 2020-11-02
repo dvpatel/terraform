432c9d10584ad8d4b3ea818fa2a71a6016cddb5e
Dipesh Patel <mr.dvpatel@gmail.com>
2020-11-01 20:50:52 -0500

    Add terraform setup

--- eks-mng/setup.sh
+++ eks-mng/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#  https://aws.amazon.com/blogs/compute/cost-optimization-and-resilience-eks-with-spot-instances/
+#  https://github.com/kubernetes/autoscaler/blob/cluster-autoscaler-release-1.18/cluster-autoscaler/cloudprovider/aws/README.md
+
+terraform init
+
+terraform apply -auto-approve
+
 aws eks --region us-east-1 update-kubeconfig --name dev-eks
 
 kubectl apply -f all-resources.yaml

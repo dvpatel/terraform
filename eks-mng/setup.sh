fdb2d25178e9e8ea18139a4028094781525d098b
Dipesh Patel <mr.dvpatel@gmail.com>
2020-11-03 00:00:55 -0500

    use private alb, add alb yaml spec

--- eks-mng/setup.sh
+++ eks-mng/setup.sh
@@ -9,12 +9,20 @@ terraform apply -auto-approve
 
 aws eks --region us-east-1 update-kubeconfig --name dev-eks
 
+#  Install cert mgr
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.2/cert-manager.yaml
+
 #  https://github.com/aws/eks-charts/tree/master/stable/aws-load-balancer-controller
 #  alb-node-iam-policy.json
 #  Set ALB Ingress, pre-req IAM Policy alb-node-iam-policy.json must be attached to eks worker nodes (devNodeInstanceRole), AWSLoadBalancerControllerIAMPolicy
 helm repo add eks https://aws.github.io/eks-charts
 kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
 helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=dev-eks
+#  kubectl -n kube-system rollout status deployment aws-load-balancer-controller
+
+
+#  Install yaml spec for alb
+kubectl apply -f install_v2_0_0.yaml
 
 
 #  Setup Node Termination handler as daemonset

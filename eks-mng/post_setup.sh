932820d82bbb9811cd42cf9dcbaa195d176067f1
Dipesh Patel <mr.dvpatel@gmail.com>
2020-11-08 18:22:02 -0500

    parameterize

--- eks-mng/post_setup.sh
+++ eks-mng/post_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-aws eks --region us-east-1 update-kubeconfig --name dev-eks
+REGION="$1"
+CLUSTER_NAME="$2"
+
+aws eks --region $REGION update-kubeconfig --name $CLUSTER_NAME
 
 #  Install cert mgr
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.2/cert-manager.yaml
@@ -14,7 +17,7 @@ helm repo add eks https://aws.github.io/eks-charts
 #  alb-node-iam-policy.json
 #  Set ALB Ingress, pre-req IAM Policy alb-node-iam-policy.json must be attached to eks worker nodes (devNodeInstanceRole), AWSLoadBalancerControllerIAMPolicy
 kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
-helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=dev-eks
+helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=$CLUSTER_NAME
 kubectl logs -n kube-system $(kubectl get po -n kube-system | egrep -o aws-load-balancer-controller[a-zA-Z0-9-]+)
 #  kubectl -n kube-system rollout status deployment aws-load-balancer-controller
 

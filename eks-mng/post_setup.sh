b540f8441f3fe06fbb108319ce707a19a01f2279
Dipesh Patel <mr.dvpatel@gmail.com>
2020-11-12 01:25:52 -0500

    change istio profile to default

--- eks-mng/post_setup.sh
+++ eks-mng/post_setup.sh
@@ -66,7 +66,7 @@ kubectl apply -f https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/v1.7.5
 
 #  istio setup during bastion install
 #  Install istio
-istioctl install --set profile=demo
+istioctl install --set profile=default
 kubectl -n istio-system get svc
 kubectl -n istio-system get pods
 

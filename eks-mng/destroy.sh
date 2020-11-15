19acc53db9300238299d3c0d209ddd60fe851e94
Dipesh Patel <mr.dvpatel@gmail.com>
2020-11-15 12:35:20 -0500

    add logic to uninstall istio

--- eks-mng/destroy.sh
+++ eks-mng/destroy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 kubectl delete -f web-app.yaml 
+
+#  Uninstall istio
+kubectl delete -f samples/addons
+istioctl manifest generate --set profile=demo | kubectl delete --ignore-not-found=true -f -
+kubectl delete namespace istio-system
+
 terraform destroy -auto-approve

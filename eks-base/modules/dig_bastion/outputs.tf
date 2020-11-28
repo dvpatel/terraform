3f287b027f60e1ccad01e65e006cc1e7063f1a91
Dipesh Patel <mr.dvpatel@gmail.com>
2020-11-27 20:25:52 -0500

    output public ip

--- eks-base/modules/dig_bastion/outputs.tf
+++ eks-base/modules/dig_bastion/outputs.tf
@@ -1,11 +1,11 @@
 # Output variable definitions
 
-output "public_ip" {
+output "bastion_ip" {
   description = "Bastion IP"
   value       = module.bastion_instance.public_ip
 }
 
-output "instance_public_dns" {
+output "bastion_dns" {
   description = "Public DNS name assigned to the EC2 instance"
-  value       = module.bastion_instance.public_dns[0]
+  value       = module.bastion_instance.public_dns
 }
--- eks-base/outputs.tf
+++ eks-base/outputs.tf
@@ -1,9 +1,9 @@
-# output "public_ip" {
-#   description = "Bastion Public IP"
-#   value       = module.app_bastion.public_ip
-# }
+output "bastion_ip" {
+  description = "Bastion Public IP"
+  value       = module.app_bastion[0].bastion_ip[0]
+}
 
-# output "instance_public_dns" {
-#   description = "Public DNS name assigned to the EC2 instance"
-#   value       = module.app_bastion.public_dns[0]
-# }
+output "bastion_dns" {
+  description = "Public DNS name assigned to the EC2 instance"
+  value       = module.app_bastion[0].bastion_dns[0]
+}

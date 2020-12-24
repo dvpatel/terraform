65736fa665ed951789af56c13bf7d8323343cb17
Dipesh Patel <mr.dvpatel@gmail.com>
2020-12-24 18:29:33 -0500

    remove deprecated fields

--- eks-base/main.tf
+++ eks-base/main.tf
@@ -3,21 +3,17 @@ terraform {
 }
 
 provider "aws" {
-  version = ">= 2.28.1"
   region  = var.region
   profile = var.profile
 }
 
 provider "local" {
-  version = "~> 1.2"
 }
 
 provider "null" {
-  version = "~> 2.1"
 }
 
 provider "template" {
-  version = "~> 2.1"
 }
 
 module "vpc_eks" {
